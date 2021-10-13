63f45b6cad0d67ab1e2c956fbad096593f901f41
Florestan Bredow <florestan@bredow.fr>
2021-10-13 15:16:04 +0200

    Add color to crossroad templates

--- actions/crossroad.php
+++ actions/crossroad.php
@@ -3,16 +3,16 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$link = $this->getParameter('link');
-$title = $this->getParameter('title');
-$text = $this->getParameter('text');
-//$image = $this->getParameter('image');
-$image = "crossroad-default-image.png";
+$link = $this->getParameter('link', 'http://perdu.com');
+$title = $this->getParameter('title', 'Titre à ajouter');
+$text = $this->getParameter('text', 'Description à ajouter.');
+$color = $this->getParameter('color', 'social');
+$image = $this->getParameter('image', 'crossroad-default-image.png');
 
 print("<article class='crossroad-item'>
-  <h1>{$title}</h1>
+  <h1 class='{$color}' white top left no-repeat;'>{$title}</h1>
   <img src='custom/themes/ecoresponsables/images/{$image}' />
   <p>{$text}</p> 
-  <a class='crossroad-button' href='?{$link}'>Lire la suite</a>
+  <a class='crossroad-button {$color}' href='?{$link}'>Lire la suite</a>
 </article>
 ");
