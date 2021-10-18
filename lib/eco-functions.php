993dd1bdb5141c0523802c71263abece5b84497d
Florestan Bredow <florestan@bredow.fr>
2021-10-18 14:27:53 +0200

    Refactore

--- lib/eco-functions.php
+++ lib/eco-functions.php
@@ -1,25 +1,58 @@
 <?php
 
+const ECO_COLORS = array("social", "energy", "nature", "water");
+
 function eco_color_from_title(string $title): string {
-    $list_colors = array("social", "energy", "nature", "water");
 
     $hash = hash("crc32", $title);
     $digit_val = hexdec($hash);
-    $index = $digit_val % count($list_colors);
+    $index = $digit_val % count(ECO_COLORS);
 
-    $color = $list_colors[$index];
+    $color = ECO_COLORS[$index];
     return $color;
 }
 
-function eco_resize_image_and_cached(string $image_file, int $with, int $height, string $default_result): string {
-    if (isset($fiche['imagethumbs']) and !empty($fiche['imagethumbs'])) {
-        $imageUri = redimensionner_image(
-          "files/{$fiche['imagethumbs']}",
-          "cache/image_{$thumb_size}_{$thumb_size}_{$fiche['imagethumbs']}",
-          $thumb_size, $thumb_size, 'crop'
-        ); 
-        if (empty($imageUri) !== true) {
-          $image = "<img src='{$imageUri}'/>\n";
-        }
+function eco_resize_image_and_cached(string|null $image, int $width, int $height, string $default_image): string {
+    if (!isset($image) or empty($image)) {
+        return $default_image;
     }
+
+    $resized_image = redimensionner_image(
+        "files/{$image}",
+        "cache/image_{$width}_{$height}_{$image}",
+        $width, 
+        $height,
+        'crop'
+    );
+
+    if (empty($resized_image) === true) {
+      return $default_image;
+    }
+
+    return $resized_image;
 }
+
+function eco_format_date(string $datetime): string {
+    setlocale(LC_TIME, 'fr_FR.UTF-8');
+    $dateFormat = "%e %B %G";
+    return strftime($dateFormat, strtotime($datetime));
+}
+
+function eco_date_string(array $fiche): string {
+    $date = 'Créé le ' . eco_format_date($fiche['date_creation_fiche']);
+    if ($fiche['date_creation_fiche'] !== $fiche['date_maj_fiche']) {
+      $date .= ' / Mis à jour le ' . eco_format_date($fiche['date_maj_fiche']);
+    }
+
+    return $date;
+}
+
+function eco_get_theme_path(): string {
+    $themePath = "themes/ecoresponsables/";
+    $altThemePath = "custom/{$themePath}";
+    if (is_dir($altThemePath) === true) {
+        $themePath = $altThemePath;
+    }
+    return $themePath;
+}
+
